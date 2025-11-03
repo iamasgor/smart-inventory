20d04bee6dff762d285f291b7591cc896650c3eb
Ali Asgor <iamasgor@yahoo.com>
2025-11-03 17:01:30 +0600

    urls and template added

--- inventoryproj/settings.py
+++ inventoryproj/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'adminapp'
+    'accounts',
+    'api',
+    'core',
+    'products',
+    'purchases',
+    'reports',
+    'sales',
 ]
 
 MIDDLEWARE = [
---
+++ reports/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from reports import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
