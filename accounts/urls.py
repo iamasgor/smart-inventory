fbe9b1351262c1cc520d37e275fd91dfaf8930d5
Ali Asgor <iamasgor@yahoo.com>
2025-11-12 16:26:55 +0600

    login add

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('', views.index_view, name='index'),
+    path('login/', views.login_view, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,24 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
-def index(request):
+@login_required(login_url='login')
+def index_view(request):
     return render(request, 'accounts/index.html')
 
 
-def login(request):
-    return render(request, 'accounts/index.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return render(request, 'accounts/index.html')
+        else:
+            messages.error(request, "Invalid username or password")
+    return render(request, 'accounts/login.html')
